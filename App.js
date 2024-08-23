388dfa371504e1c68fa22519d7f77d351a0c3d8c
bae080311 <s24024@gsm.hs.kr>
2024-08-23 20:39:28 +0900

    feat: todo style, add todofunction

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { theme } from "./assets/color";
-import { TextInput } from "react-native-web";
+
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
@@ -10,9 +17,14 @@ export default function App() {
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
-  const addTodo = () => {
-    if (text === "") return;
-    const newToDos = { ...toDos, [Date.now()]: { text, work: working } };
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, work: working },
+    };
     setToDos(newToDos);
     setText("");
   };
@@ -20,34 +32,42 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={work}>
           <Text
-            onPress={work}
             style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
           >
             Work
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
           <Text
-            onPress={travel}
-            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
           >
             Travel
           </Text>
         </TouchableOpacity>
       </View>
       <TextInput
-        onSubmitEditing={addTodo}
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
-        ketboardType="web-search"
+        returnKeyType="done"
+        value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         style={styles.input}
-      ></TextInput>
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,19 +77,30 @@ const styles = StyleSheet.create({
   header: {
     justifyContent: "space-between",
     flexDirection: "row",
-    flex: 1,
     marginTop: 100,
   },
   btnText: {
     fontSize: 38,
-    fontWeight: 600,
+    fontWeight: "600",
   },
   input: {
     backgroundColor: "white",
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
